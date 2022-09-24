b4a4ccb3050a8a6d7598d794e1b9100654ba856a
Julian <iulian.corut10@gmail.com>
2022-09-24 11:09:38 +0100

    Project Rock, Paper, Scissors is completely functional. Need to implement CSS.

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# rock-paper-scissors
+#The Odin Project: Rock, Paper, Scissors
+
+## Instructions
+
+The Odin Project (Foundations): https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/dom-manipulation#dom-methods
+
+## Purpose: 
+To create and display on an UI using DOM Manipulation Methods the well-known game 'Rock, Paper, Scissors.
+
+## Preview:
+Work in progress... 
+Need to implement CSS.
+
+### Software: 
+* JavaScript
+* HTML
+* CSS (to be implemented)
+
+
+### Tools
+* GitHub
+* StackOverFlow
+
+### Result :green_check_mark:
+I made the well-known game: Rock, Paper, Scissors by using the JavaScript program. The game takes the player input by clicking one of the three options and then gives a randomised computer result and checks who is the winner of the round. Also, whoever gets the score of 5 first, the program nominates him as the winner. 
+
+
+| Website | Link | 
+| ------------- | ------------- | 
+| CodePen demo | https://codepen.io/juliancrt/pen/dyeVEVR | 
+
+
+
+## Summary
+
+### What I have learned.
+
+a. Be as descriptive and concise as possible when naming your function names. eg. getComputerChoice. This way, you won't need to add too many comments to explain your work.
+
+b. Create a function for every action. Clean coding is very important in order to be easy to debugg and for other coders to understand it. 
+
+b1. Test your functions everytime before moving on so you will make sure it work (console.log does wonders).
+
+c. Registering HTML buttons to JavaScript Functions. It was the hardest problem I had to overcome during this project (I did it!!).
+
+d. Creating elements in JavaScript and appending them to the HTML. 
--- script.js
+++ script.js
@@ -1,86 +1,99 @@
-const choices = ["rock", "paper", "scissors"];
-const winners = [];
+/* --- Set up the variables --- */
 
+let playerScore = 0;
+let ties = 0;
+let computerScore = 0;
 
+/* --- Constants created along the process --- */
 
-function game() {
-   for(i = 1; i <= 5; i++){
-     playRound(i);
-    }
-    logWins();
-     
+const choices = ['rock', 'paper', 'scissors']
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const outcomeDiv = document.querySelector('.outcome');
+const roundWinner = document.querySelector('round-winner')
+const p = document.createElement('p')
+const buttons = document.querySelectorAll('#buttons')
+
+/* --- Create function to disable the buttons after 5 rounds, thus stopping the game --- */
+
+function disableButtons() {
+  buttons.forEach(elem => {
+      elem.disabled = true
+  })
 }
 
-
-function playRound(round) {
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const winner = checkWinner(playerSelection,computerSelection);
-    winners.push(winner);
-    roundWinner(playerSelection, computerSelection, winner, round);
-}
-
-
-
-function getPlayerChoice() {
-    let input = prompt("Please choose Rock, Paper or Scissors");
-    
-    while(input==null) {
-        input = prompt("Please choose Rock, Paper or Scissors");
-    }
-    input = input.toLowerCase();
-
-    let check = validateInput(input);
-
-    while(check==false) {
-        input = prompt("Please choose Rock, Paper or Scissors. Spelling must be exact");
-
-    while(input==null) {
-        input = prompt("Please choose Rock, Paper or Scissors");
-    }
-    input = input.toLowerCase();
-    check = validateInput(input);
-    }
-    return input;
-}
+/* --- Random input for the computerChoice --- */
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
-}
+};
 
+/* --- Executes game logic, conditions for TIES and playerChoice to win versus computerChoice's random input. Score is incrementing after each round. ---*/
 
-function validateInput(choice) {
-    return choices.includes(choice);
-}
+const playRound = (computerChoice, playerChoice) => {
+  if(computerChoice === playerChoice) {
+    ties ++
+    
+    p.innerText = 'It\'s a tie'
+    document.querySelector('.ties').innerText = 'Ties: ' +ties
+    outcomeDiv.appendChild(p)
+    
+} else if ((playerChoice == 'rock' && computerChoice == 'scissors') ||
+           (playerChoice == 'paper' && computerChoice == 'rock') ||
+           (playerChoice == 'scissors' && computerChoice == 'paper')) {
+               playerScore ++
+               
+    p.innerText = `You win!  ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}  beats  ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}`
+    document.querySelector('.player-wins').innerText = 'Player Score: ' +playerScore
+    outcomeDiv.appendChild(p)
+               
+                                
+}  else {computerScore ++
+  
+    p.innerText = `You lost!  ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}  beats  ${playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1)}` 
+    document.querySelector('.computer-wins').innerText = 'Computer Score: ' +computerScore
+    outcomeDiv.appendChild(p)
+}};
 
-function checkWinner(choiceP, choiceC){
+/* --- Function that checks if computer won 'else if' player. Function called for disabling the buttons after winner has been announced (disableButtons()). --- */
 
-    if (choiceP == choiceC) {
-        return "It\'s a tie"
-    } else if ((choiceP == "rock" && choiceC == "scissors") || 
-               (choiceP == "paper" && choiceC == "rock") ||
-               (choiceP == "scissors" && choiceC == "paper")) {
-                return "Player";
-               } else {
-                return "Computer"; }
-}
+  const checkWinner = (computerScore, playerScore) => {
+    if(computerScore === 5) {
+      disableButtons()
+      outcomeDiv.removeChild(p)
+      const h2 = document.createElement('h2')
+      h2.innerText = `You lost! ${computerScore} to ${playerScore} for the computer. Better luck next time!`
+      outcomeDiv.appendChild(h2)
+      
+    } else if (playerScore === 5) {
+      disableButtons()
+      outcomeDiv.removeChild(p)
+      const h2 = document.createElement('h2')
+      h2.innerText = 'You won! Not bad rookie, see you next time!'
+      outcomeDiv.appendChild(h2)
+    }
+  }
+  
+  /* --- Event Listener functions for each button (playerChoice) + the functions needed to run the game called within them. ---*/
 
-function logWins() {
-    let playerWins = winners.filter((item) => item == "Player").length;
-    let computerWins = winners.filter((item) => item == "Computer").length;
-    let ties = winners.filter ((item) => item == "It\'s a tie").length;
-    console.log("Results:");
-    console.log("Player Wins:", playerWins);
-    console.log("Computer Wins:", computerWins);
-    console.log("Ties:", ties);
-}
+  rockButton.addEventListener('click', () => {
+  const computerChoice = getComputerChoice();
+  const playerChoice = 'rock';
+  playRound(computerChoice, playerChoice);
+  checkWinner(computerScore, playerScore);
+})
 
-function roundWinner(playerSelection, computerSelection, winner, round) {
-    console.log("Round Number:", round)
-    console.log("Player chose:", playerSelection);
-    console.log("Computer chose:", computerSelection);
-    console.log(winner, "Won this Round");
-    console.log("-------------------------");
-}
+  paperButton.addEventListener('click', () => {
+  const computerChoice = getComputerChoice();
+  const playerChoice = 'paper';
+  playRound(computerChoice, playerChoice);
+  checkWinner(computerScore, playerScore);
+})
 
-game();
+  scissorsButton.addEventListener('click', () => {
+  const computerChoice = getComputerChoice();
+  const playerChoice = 'scissors';
+  playRound(computerChoice, playerChoice);
+  checkWinner(computerScore, playerScore);
+})
